6a93f8bd24f1eb723213b4e4f8ac283d0b52c694
ISTI-Developers <isti@unmg.com.ph>
2025-09-05 10:47:08 +0800

    further changes

--- pisopayRequest.php
+++ pisopayRequest.php
@@ -23,21 +23,18 @@ $customer_name = ($_POST['first_name'] ?? 'Default') . " " . ($_POST['last_name'
 $customer_email = $_POST['email_address'] ?? 'default@example.com';
 $customer_phone = $_POST['mobile_number'] ?? '0000000000';
 $customer_address = $_POST['address'] ?? '2F HPL Building, 60 Sen. Gil Puyat Ave., Makati City 1200 Philippines';
+$amount = $_POST['amount'] ?? 0;
 
 $merchant_trace_no = "thi" . rand(0, 99999);
 
-$amount = 0;
 $f = new Checkout();
 $details = array(
-    array("name" => "Request", "price" => "1", "quantity" => "1"),
+    array("name" => "In-house Training: Gravitas Leadership Payment", "price" => $amount, "quantity" => "1"),
 );
 
 $session_id = $f->sessionGenerate();
 
 if ($session_id) {
-    $amount = 0;
-    $amount = max($amount, 0);
-
     $customer_ip = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
     $ip_address = $customer_ip ?: gethostbyname(gethostname());
 
