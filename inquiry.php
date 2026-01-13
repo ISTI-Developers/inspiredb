7191b7ec3af3e6d728f817712a24da22fa20972c
ISTI-Developers <164320922+ISTI-Developers@users.noreply.github.com>
2026-01-13 10:12:28 +0800

    addtl changes

--- inquiry.php
+++ inquiry.php
@@ -51,6 +51,90 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 exit;
             }
         }
+        if (!isset($_POST['is_recorded']) || $_POST['is_recorded'] === "") {
+            echo "Incomplete input. Missing: is_recorded";
+            exit;
+        }
+        if (!isset($_POST['is_streamed']) || $_POST['is_streamed'] === "") {
+            echo "Incomplete input. Missing: is_streamed";
+            exit;
+        }
+        $fileArray = [];
+        $errors = [];
+        if (!empty($_FILES)) {
+            foreach ($_FILES as $index => $file) {
+                if ($file['error'] === UPLOAD_ERR_OK) {
+                    $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+                    $targetPath = "/inquiries/" . date("mdYHis") . "_$index.$ext";
+                    $fileArray[] = $targetPath;
+                }
+            }
+        }
+        $urls = count($fileArray) ? json_encode($fileArray) : "";
+        $success = $inquiry->insertInquiry(
+            $_POST['company_name'],
+            $_POST['company_information'],
+            $_POST['name'],
+            $_POST['designation'],
+            $_POST['email'],
+            $_POST['contact_number'],
+            $_POST['event_title'],
+            $_POST['event_description'],
+            $_POST['event_setup'],
+            $_POST['event_location'],
+            $_POST['is_recorded'],
+            $_POST['is_streamed'],
+            $_POST['event_date'],
+            $_POST['speaker_timeslot'],
+            $_POST['audience_size'],
+            $_POST['audience_profile'],
+            $_POST['training_objective'],
+            $urls
+        );
+        if (!$success) {
+            echo "Incomplete input.";
+            exit;
+        }
+        foreach ($_FILES as $index => $file) {
+            $i = array_search($targetPath, $fileArray);
+            if (!move_uploaded_file($file['tmp_name'], "." . $fileArray[$i])) {
+                $errors[] = 0;
+            } else {
+                $errors[] = 1;
+            }
+        }
+        if (in_array(0, $errors)) {
+            echo "Error on file uploading";
+            exit;
+        }
+        $message1 = file_get_contents('./templates/inquiries.php');
+        $message1 = str_replace("[name]", $_POST['name'], $message1);
+        $message2 = file_get_contents('./templates/ask.php');
+        foreach ($_POST as $key => $value) {
+            if ($key === 'is_recorded' || $key === 'is_streamed') {
+                $value = $value === "1" ? "Yes" : "No";
+            }
+            $message2 = str_replace("[$key]", $value, $message2);
+        }
+        $mail1 = $inquiry->sendMail(
+            "Thank you for your Inquiry!",
+            $message1,
+            $_POST['email'],
+            $_POST['name']
+        );
+        $mail2 = $inquiry->sendMail(
+            "New Inquiry!",
+            $message2,
+            "ask@inspireleaders.com.ph",
+            "",
+            NULL,
+            json_decode($urls, true)
+        );
+        if ($mail1 && $mail2) {
+            echo "Thank you for your inquiry! We'll get back to you shortly.";
+        } else {
+            echo "Error sending email. Please try again later.";
+        }
 
         // Validate radios (special case because "0" is a VALID VALUE)
         if (!isset($_POST['is_recorded']) || $_POST['is_recorded'] === "") {
